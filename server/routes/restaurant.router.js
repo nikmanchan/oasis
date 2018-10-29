3542a9de5627dcb6918e9799384c11513111497a
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-10-29 15:30:55 -0500

    add restaurant server-side POST to database

--- server/routes/restaurant.router.js
+++ server/routes/restaurant.router.js
@@ -18,10 +18,16 @@ router.get('/', (req, res) => {
 });
 
 /**
- * POST route template
+ * POST new restaurant
  */
 router.post('/', (req, res) => {
-
+    const query = `INSERT INTO "restaurant" ("name", "latitude", "longitude", "restriction", "friendliness", 
+    "costliness", "comments", "image_url")
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8);`;
+    pool.query(query, [req.body.name, req.body.latitude, req.body.longitude, req.body.restriction,
+    req.body.friendliness, req.body.costliness, req.body.comments, req.body.image_url]).then(() => {
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log('Error with restaurant POST to database: ', error);
+    });
 });
-
-module.exports = router;
