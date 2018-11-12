7f13cbe1ae46c5ca3930e3c00bfb61cc2382f665
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-11-11 22:39:51 -0600

    add spacing for restaurant detail card

--- src/components/RestaurantDetail/RestaurantDetail.js
+++ src/components/RestaurantDetail/RestaurantDetail.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button, Card, CardImg, CardText, CardBody, CardLink,
     CardTitle, CardSubtitle } from 'reactstrap';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
-// import { Button } from 'reactstrap';
+
 import { withRouter } from 'react-router-dom';
 import './RestaurantDetail.css';
 
@@ -55,10 +51,11 @@ class RestaurantDetail extends Component {
                         
                     <CardBody>
                         <h3>{this.props.state.restaurantDetails[0].name}</h3>
-                        <p>{this.props.state.restaurantDetails[0].address}</p>
-                        <p>
+                        <p>{this.props.state.restaurantDetails[0].address} 
+                        <br></br>
                         <a href={this.props.state.restaurantDetails[0].menu_url} target="_blank">Menu</a>
                         &ensp;
+                        {/* &emsp; */}
                         <a href={`https://www.google.com/maps/dir/?api=1&destination=${this.props.state.restaurantDetails[0].address}`} target="_blank">Get Directions</a>
                         </p>
                     </CardBody>
