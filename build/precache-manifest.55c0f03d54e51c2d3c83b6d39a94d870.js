58b48b13401a23320b0b28e9a4cc5940a15d9ebb
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-12-04 23:10:21 -0600

    add build file for heroku

---
+++ build/precache-manifest.55c0f03d54e51c2d3c83b6d39a94d870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9337a569a17c098a823c",
+    "url": "/static/js/main.9337a569.chunk.js"
+  },
+  {
+    "revision": "0167a1268565295dc650",
+    "url": "/static/js/1.0167a126.chunk.js"
+  },
+  {
+    "revision": "9337a569a17c098a823c",
+    "url": "/static/css/main.10a4ecd9.chunk.css"
+  },
+  {
+    "revision": "0167a1268565295dc650",
+    "url": "/static/css/1.196683a2.chunk.css"
+  },
+  {
+    "revision": "ae2949627d174264d5215ecf92e7adb2",
+    "url": "/index.html"
+  }
+];
